206bc8e8400e07b88a60fbf95f906ef299b7344a
abdallah <abdallahelmekawy6@gmail.com>
2024-01-12 16:56:46 +0200

    rendering statuses <a> tags query string in the view

--- resources/views/livewire/ideas/status-filters.blade.php
+++ resources/views/livewire/ideas/status-filters.blade.php
@@ -1,18 +1,20 @@
 <div>
     <nav class="hidden sm:flex items-center justify-between text-xs">
         <ul class="uppercase font-semibold flex space-x-6 rtl:space-x-reverse pb-3 border-b-4 border-gray-20">
-            <li><a href="" wire:click.prevent="setStatus('All')" @class([
-                'pb-3 border-b-4 transition ease-in duration-150 hover:border-blue',
-                'border-blue text-gray-900' => $currentStatus === 'All',
-                'text-gray-400' => $currentStatus !== 'All',
-            ])>All ideas
+            <li><a href="{{ route('idea.index', ['status' => 'All']) }}" wire:click.prevent="setStatus('All')"
+                    @class([
+                        'pb-3 border-b-4 transition ease-in duration-150 hover:border-blue',
+                        'border-blue text-gray-900' => $currentStatus === 'All',
+                        'text-gray-400' => $currentStatus !== 'All',
+                    ])>All ideas
                     ({{ $statusCount['total'] }})</a>
             </li>
             @foreach ($statuses as $status)
                 @if ($loop->iteration > 3)
                 @break
             @endif
-            <li><a href="" wire:click.prevent="setStatus('{{ $status->name }}')"
+            <li><a href={{ route('idea.index', ['status' => $status->name]) }}
+                    wire:click.prevent="setStatus('{{ $status->name }}')"
                     @class([
                         'pb-3 border-b-4 transition ease-in duration-150 hover:border-blue',
                         'border-blue text-gray-900' => $currentStatus === $status->name,
